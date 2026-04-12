becd62a3f22f5260eff8f351ae0b98f40481cae1
ptho1504 <phuoctho150420@gmail.com>
2026-04-12 14:58:52 +0700

    add outbound for ec2, alb

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -9,6 +9,13 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
@@ -22,6 +29,13 @@ resource "aws_security_group" "ec2_sg" {
     to_port         = 80
     protocol        = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
